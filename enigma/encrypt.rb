e763ee3d5536fdf0e591c0b9f621eebb4fbce68a
Kenny <chenkenn@gmail.com>
2015-03-07 09:31:56 -0700

    encrypter complete

--- enigma/encrypt.rb
+++ enigma/encrypt.rb
@@ -1,11 +1,11 @@
 require "./rotator"
 
 class Encrypt
-  attr_reader :read_doc, :key, :date
-  attr_accessor :write_doc
+  attr_reader :key, :date
+  attr_accessor :write_doc, :read_doc
 
   def initialize(read_doc = nil, write_doc = nil, key = nil, date =nil)
-    read_doc ? @read_doc = read_doc : @read_doc = File.read(ARGV[0])
+    read_doc ? @read_doc = read_doc : @read_doc = File.read(ARGV[0]).strip
     write_doc ? @write_doc = write_doc : @write_doc = (ARGV[1])
     key ? @key = key : @key = ARGV[2]
     date ? @date = date : @date = ARGV[3]
@@ -14,8 +14,11 @@ class Encrypt
 
   def write_encrypted_file
     output = File.open(@write_doc, "w")
-    output.write(@rotator.encrypt_rotate_characters.join)
+    encrypted_text = @rotator.encrypt_rotate_characters.join
+    output.write(encrypted_text)
     output.close
   end
 
 end
+
+Encrypt.new.write_encrypted_file
